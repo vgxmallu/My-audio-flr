4a773940408accf77837c46c3810848b121c4be7
vgxmallu <DeltaAlgor@outlook.com>
2025-07-30 08:13:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -769,8 +769,8 @@ async def auto_filter(client, msg, spoll=False):
                 caption=cap[:1024],
                 reply_markup=InlineKeyboardMarkup(btn)
             )
-            await asyncio.sleep(300)
-            await delauto.delete()
+            #await asyncio.sleep(300)
+            #await delauto.delete()
         except (MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty):
             pic = imdb.get('poster')
             poster = pic.replace('.jpg', "._V1_UX360.jpg")
@@ -779,13 +779,13 @@ async def auto_filter(client, msg, spoll=False):
                 caption=cap[:1024],
                 reply_markup=InlineKeyboardMarkup(btn)
             )
-            await asyncio.sleep(300)
-            await delau.delete()
+            #await asyncio.sleep(300)
+            #await delau.delete()
         except Exception as e:
             logger.exception(e)
             audel = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
-            await asyncio.sleep(300)
-            await audel.delete()
+            #await asyncio.sleep(300)
+            #await audel.delete()
     else:
         if HYPER_MODE:
             autodel = await message.reply_text(
@@ -797,11 +797,11 @@ async def auto_filter(client, msg, spoll=False):
         else:
             autodel = await message.reply_text(cap, reply_markup=InlineKeyboardMarkup(btn))
 
-        await asyncio.sleep(300)
-        await autodel.delete()
+        #await asyncio.sleep(300)
+        #await autodel.delete()
 
     if spoll:
-        #await msg.message.delete()
+        await msg.message.delete()
 
 #SPELL CHECK RE EDITED BY GOUTHAMSER
 async def advantage_spell_chok(client, msg):
